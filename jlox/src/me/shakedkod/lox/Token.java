e5d7f8f13da592b9a305c9f0b0d9687f9c5f51ad
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2024-07-30 17:56:49 +0300

    chapter 4.2 done

---
+++ jlox/src/me/shakedkod/lox/Token.java
@@ -0,0 +1,22 @@
+package me.shakedkod.lox;
+
+public class Token
+{
+    final TokenType _type;
+    final String    _lexeme;
+    final Object    _literal;
+    final int       _line;
+
+    public Token(TokenType type, String lexeme, Object literal, int line)
+    {
+        _type = type;
+        _lexeme = lexeme;
+        _literal = literal;
+        _line = line;
+    }
+
+    public String toString()
+    {
+        return _type + " " + _lexeme + " " + _literal;
+    }
+}
