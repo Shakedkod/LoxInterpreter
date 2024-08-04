02dfcd3c0f20837d92858006f1b4d238d7d6fc7c
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2024-08-04 18:00:18 +0300

    chapter 17.3 done

--- clox/chunk.h
+++ clox/chunk.h
@@ -6,9 +6,20 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum
 {
+    // value
+    OP_CONSTANT,
+    // unary operation
+    OP_NEGATE,
+    // binary operation
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    // return
     OP_RETURN
 } OpCode;
 
@@ -17,10 +28,14 @@ typedef struct
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
 void freeChunk(Chunk* chunk);
+
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
---
+++ clox/debug.h
@@ -0,0 +1,13 @@
+//
+// Created by ShakedKod on 3/8/2024.
+//
+
+#ifndef clox_debug_h
+#define clox_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
